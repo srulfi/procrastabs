5c2f6d9cb98fbd3e0ade43efeb53e8146794412b
Santiago Rulfi <santiagorulfi@gmail.com>
2022-10-28 10:45:58 -0300

    Add defaults file

---
+++ defaults.js
@@ -0,0 +1,16 @@
+export default {
+	badge: {
+		baseColor: "#90EE90",
+		countdownColor: "#B81D13",
+		countdownSeconds: 60,
+		countdownEnabled: true,
+	},
+	maxTabs: {
+		value: 10,
+		enabled: false,
+	},
+	countdown: {
+		value: 1,
+		enabled: false,
+	},
+}
